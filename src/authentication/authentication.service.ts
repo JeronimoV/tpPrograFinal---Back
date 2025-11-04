269d73cc121d1dced6f486afd092147259e39d82
JeronimoV <jeronimoevilar@outlook.com>
2025-11-04 15:49:36 -0300

    probando9

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -3,7 +3,7 @@ import { Users } from '../users/schema/users.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from "bcrypt"
-import { AuthService } from 'src/utils/auth/auth.service';
+import { AuthService } from '../utils/auth/auth.service';
 
 @Injectable()
 export class AuthenticationService {
