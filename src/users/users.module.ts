5e155ee2895f1a5ff9fb219750f99fe020ebc99b
JeronimoV <jeronimoevilar@outlook.com>
2025-11-04 16:01:26 -0300

    probando12

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthenticationModule } from 'src/authentication/authentication.module';
 import { CloudinaryModule } from 'src/utils/cloudinary/cloudinary.module';
+import { AuthModule } from 'src/utils/auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]), AuthenticationModule, CloudinaryModule],
+  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UsersSchema }]), AuthenticationModule, CloudinaryModule, AuthModule],
   controllers: [UsersController],
   providers: [UsersService]
 })
