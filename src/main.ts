543e208a8fa6e8cc3e030a542f076eab69a8075e
JeronimoV <jeronimoevilar@outlook.com>
2025-11-12 09:23:36 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: "https://tp-progra-final-front.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
