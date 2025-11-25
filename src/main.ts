bdd111a61317592931209ad9be3f6008de494897
JeronimoV <jeronimoevilar@outlook.com>
2025-11-25 02:03:32 -0300

    test2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "*",
+    origin: "http://localhost:4200",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
